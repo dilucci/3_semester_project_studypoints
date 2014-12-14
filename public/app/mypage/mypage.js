11a40688fa09611f54e37a8a0fb72118b5abda21
Michael <larsen_max@hotmail.com>
2014-12-14 07:34:34 +0100

    addet html til at user kan ændre pw

--- public/app/mypage/mypage.js
+++ public/app/mypage/mypage.js
@@ -9,7 +9,37 @@ angular.module('myAppRename.mypage', ['ngRoute'])
         });
     }])
     .controller('mypageCtrl', ['$scope', '$http', function ($scope, $http) {
-        //console.log("inde i mypageCtrl")
+        $scope.error = null;
+        $scope.changePassword = function () {
+            console.log('user.password: ' + $scope.user.password);
+            console.log('oldpw: ' + $scope.oldPassword);
+            console.log('newpw: ' + $scope.newPassword);
+            if($scope.oldPassword === $scope.user.password && $scope.newPassword === $scope.confirmPassword){
+                console.log("POSTING NEW PASSWORD!");
+                $scope.user.password = $scope.newPassword;
+                console.log('user new pass: ' +  $scope.user.password);
+                $http({
+                    method: 'POST',
+                    url: 'http://gruppe4.cloudapp.net/changepw',
+                    data: $scope.user
+                })
+                    .success(function (data, status, headers, config) {
+                        console.log("success!");
+                        $scope.classes.push(data);
+                        $scope.error = null;
+                    }).
+                    error(function (data, status, headers, config) {
+                        if (status == 401) {
+                            $scope.error = "You are not authenticated to request these data";
+                            return;
+                        }
+                        $scope.error = data;
+                    });
+            }
+            else{
+                $scope.error = 'Something went wrong!';
+            }
+        };
     }]);
 
 /*
